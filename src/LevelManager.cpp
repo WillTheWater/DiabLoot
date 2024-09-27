f4e3a9de2399d45796306a34f453862428be6dbc
Selbestwahr <merkabastudios@gmail.com>
2024-09-27 22:25:06 +1200

    Added new maps and updated chest locations

--- src/LevelManager.cpp
+++ src/LevelManager.cpp
@@ -10,9 +10,34 @@ LevelManager::LevelManager(System& system)
 	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_ONE, mSystem));
 	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_TWO, mSystem));
 	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_THREE, mSystem));
-	mLevels[LEVELS::LEVEL_ONE]->SpawnChest({ 500.f, 300.f }, true);
-	mLevels[LEVELS::LEVEL_TWO]->SpawnChest({ 700.f, 800.f }, false);
-	mLevels[LEVELS::LEVEL_THREE]->SpawnChest({ 800.f, 400.f }, false);
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_FOUR, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_FIVE, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_SIX, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_SEVEN, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_EIGHT, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_NINE, mSystem));
+	mLevels.push_back(std::make_unique<Level>(LEVELS::LEVEL_TEN, mSystem));
+	mLevels[LEVELS::LEVEL_ONE]->SpawnChest({ 555.f, 250.f }, true);
+	mLevels[LEVELS::LEVEL_ONE]->SpawnChest({ 1400.f, 725.f }, false);
+	mLevels[LEVELS::LEVEL_TWO]->SpawnChest({ 530.f, 700.f }, true);
+	mLevels[LEVELS::LEVEL_TWO]->SpawnChest({ 1250.f, 300.f }, false);
+	mLevels[LEVELS::LEVEL_THREE]->SpawnChest({ 200.f, 980.f }, true);
+	mLevels[LEVELS::LEVEL_THREE]->SpawnChest({ 1170.f, 170.f }, true);
+	mLevels[LEVELS::LEVEL_FOUR]->SpawnChest({ 1170.f, 560.f }, false);
+	mLevels[LEVELS::LEVEL_FIVE]->SpawnChest({ 1200.f, 920.f }, false);
+	mLevels[LEVELS::LEVEL_FIVE]->SpawnChest({ 1120.f, 430.f }, true);
+	mLevels[LEVELS::LEVEL_SIX]->SpawnChest({ 520.f, 320.f }, true);
+	mLevels[LEVELS::LEVEL_SIX]->SpawnChest({ 820.f, 470.f }, false);
+	mLevels[LEVELS::LEVEL_SIX]->SpawnChest({ 1650.f, 980.f }, true);
+	mLevels[LEVELS::LEVEL_SEVEN]->SpawnChest({ 1000.f, 180.f }, true);
+	mLevels[LEVELS::LEVEL_SEVEN]->SpawnChest({ 600.f, 800.f }, false);
+
+	mLevels[LEVELS::LEVEL_EIGHT]->SpawnChest({ 800.f, 400.f }, false); // Fix this one later
+	
+	mLevels[LEVELS::LEVEL_NINE]->SpawnChest({ 340.f, 330.f }, true);
+	mLevels[LEVELS::LEVEL_NINE]->SpawnChest({ 1450.f, 320.f }, false);
+	mLevels[LEVELS::LEVEL_TEN]->SpawnChest({ 1180.f, 480.f }, false);
+	mLevels[LEVELS::LEVEL_TEN]->SpawnChest({ 600.f, 350.f }, true);
 	assert(mLevels.size() == LEVELS::MAX_LEVELS && "LevelManager did not make the right number of levels!");
 }
 
